58a6c0fa5e3933c293c3f3a42385b3386843d37f
yundh <yun.dh@deltaindex.kr>
2021-06-17 10:47:46 +0900

    client test

---
+++ cmake/build/CMakeFiles/hw_grpc_proto.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw_grpc_proto.dir/bossopenolt.grpc.pb.cc.o"
+  "CMakeFiles/hw_grpc_proto.dir/bossopenolt.grpc.pb.cc.o.d"
+  "CMakeFiles/hw_grpc_proto.dir/bossopenolt.pb.cc.o"
+  "CMakeFiles/hw_grpc_proto.dir/bossopenolt.pb.cc.o.d"
+  "bossopenolt.grpc.pb.cc"
+  "bossopenolt.grpc.pb.h"
+  "bossopenolt.pb.cc"
+  "bossopenolt.pb.h"
+  "libhw_grpc_proto.a"
+  "libhw_grpc_proto.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw_grpc_proto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
